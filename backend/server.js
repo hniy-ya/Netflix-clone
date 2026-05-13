106ce0f7be320c74f74dc446607a578a15721f28
Haniya Ariyil <ariyihaniya@gmail.com>
2026-05-13 11:16:30 +0530

    vercel setup

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,32 @@
-import express from 'express';
+import express from "express";
 import authRoutes from "./routes/auth.route.js";
-import movieRoutes from './routes/movie.route.js';
-import tvRoutes from './routes/tv.route.js';
-import searchRoutes from './routes/search.route.js';
-import { ENV_VARS } from './config/envVars.js';
-import { connectDB } from './config/db.js';
-import { protectRoute } from './middleware/protectrroute.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-;
-const app=express();
+import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import searchRoutes from "./routes/search.route.js";
 
-const PORT= ENV_VARS.PORT;
-const MONGO_URL=ENV_VARS.MONGO_URL;
-const __dirname=path.resolve();
+import { protectRoute } from "./middleware/protectrroute.js";
+import { connectDB } from "./config/db.js";
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
 
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movie", protectRoute,movieRoutes);
-app.use("/api/v1/tv",protectRoute,tvRoutes);
-app.use("/api/v1/search",protectRoute,searchRoutes);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
-if (ENV_VARS.NODE_ENV === "production") {
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
-
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    }
-)}
-
-
-
-
-console.log("MONGO_URL:",MONGO_URL);
-
-
-app.listen(PORT, ()=>{
-    console.log('server started at http://localhost:'+PORT);
-    connectDB();
-})
-
-
+export default app;
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ export const generateTokenAndSetCokkie=(userId,res)=>{
     res.cookie("jwt-netflix",token,{
         maxAge:15 * 24 *60 * 60 *1000,
         httpOnly:true,
-        sameSite:"strict",
+        sameSite:"none",
         secure:ENV_VARS.NODE_ENV!=="development", 
     });
     return token;
