7e549d79f8d76c1899bdffdf1dc018b0ce645427
Haniya Ariyil <ariyihaniya@gmail.com>
2026-05-13 13:30:46 +0530

    token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,8 +6,8 @@ export const generateTokenAndSetCokkie=(userId,res)=>{
     res.cookie("jwt-netflix",token,{
         maxAge:15 * 24 *60 * 60 *1000,
         httpOnly:true,
-        sameSite:"none",
-        secure:ENV_VARS.NODE_ENV!=="development", 
+        sameSite:"lax",
+        secure:true, 
     });
     return token;
 };
